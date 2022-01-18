3c58deb1d10b1160b41329fc73b769702ede97a9
Ferreira-R <rrferreira99@outlook.com>
2022-01-18 11:57:34 -0500

    Encriptador funcional

---
+++ script.js
@@ -0,0 +1,55 @@
+/* Reglas de encriptación:
+"e" es convertido para "enter"
+"i" es convertido para "imes"
+"a" es convertido para "ai"
+"o" es convertido para "ober"
+"u" es convertido para "ufat"
+Solo letras minusculas
+No se permite acentuación de palabras
+*/
+
+/* Reglas de desencriptación:
+"enter" es convertido para "e"
+"imes" es convertido para "i"
+"ai" es convertido para "a"
+"ober" es convertido para "o"
+"ufat" es convertido para "u"
+Solo letras minusculas
+No se permite acentuación de palabras
+*/
+
+let frase = document.getElementById('input-texto');
+let btnEncriptar = document.getElementById('btn-encriptar');
+let btnDesencriptar = document.getElementById('btn-desencriptar');
+let fraseEncriptada = document.getElementById('msg');
+
+btnEncriptar.addEventListener('click', (event) => {
+	event.preventDefault();
+	let finalMsg = encrypt();
+	fraseEncriptada.value = finalMsg;
+});
+
+function encrypt() {
+	let originalMsg = frase.value;
+	let encryptedMsg = '';
+	encryptedMsg = originalMsg.replace(/a|e|i|o|u/g, function (char) {
+		switch (char) {
+			case 'a':
+				char = 'ai';
+				return char;
+			case 'e':
+				char = 'enter';
+				return char;
+			case 'i':
+				char = 'imes';
+				return char;
+			case 'o':
+				char = 'ober';
+				return char;
+			case 'u':
+				char = 'ufat';
+				return char;
+		}
+	});
+	return encryptedMsg;
+}
