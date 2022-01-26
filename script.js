7fe21460d5d3ecde04eace3467c6801179bb8801
Ferreira-R <rrferreira99@outlook.com>
2022-01-25 21:41:00 -0500

    Añadida validación de entrada de texto

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ Solo letras minusculas
 No se permite acentuación de palabras
 */
 
-let frase = document.getElementById('input-texto');
-let btnEncriptar = document.getElementById('btn-encriptar');
-let btnDesencriptar = document.getElementById('btn-desencriptar');
-let fraseEncriptada = document.getElementById('msg');
-let btnCopiar = document.getElementById('btn-copy');
+const frase = document.getElementById('input-texto');
+const btnEncriptar = document.getElementById('btn-encriptar');
+const btnDesencriptar = document.getElementById('btn-desencriptar');
+const fraseEncriptada = document.getElementById('msg');
+const btnCopiar = document.getElementById('btn-copy');
 
-function encryptRules(char) {
+/* function encryptRules(char) {
 	switch (char) {
 		case 'a':
 			char = 'ai';
@@ -42,9 +42,9 @@ function encryptRules(char) {
 			char = 'ufat';
 			return char;
 	}
-}
+} */
 
-function decryptRules(word) {
+/* function decryptRules(word) {
 	switch (word) {
 		case 'ai':
 			char = 'a';
@@ -62,20 +62,37 @@ function decryptRules(word) {
 			char = 'u';
 			return char;
 	}
-}
+} */
 
-function encrypt() {
-	let originalMsg = frase.value;
-	let encryptedMsg = originalMsg.replace(/a|e|i|o|u/g, encryptRules);
+const encryptRules = (char) => {
+	const rules = {
+		a: 'ai',
+		e: 'enter',
+		i: 'imes',
+		o: 'ober',
+		u: 'ufat',
+	};
+	return rules[char];
+};
+
+const decryptRules = (char) => {
+	const rules = {
+		ai: 'a',
+		enter: 'e',
+		imes: 'i',
+		ober: 'o',
+		ufat: 'u',
+	};
+	return rules[char];
+};
+
+function encrypt(msg) {
+	const encryptedMsg = msg.replace(/a|e|i|o|u/g, encryptRules);
 	return encryptedMsg;
 }
 
-function decrypt() {
-	let originalMsg = frase.value;
-	let decryptedMsg = originalMsg.replace(
-		/ai|enter|imes|ober|ufat/g,
-		decryptRules
-	);
+function decrypt(msg) {
+	const decryptedMsg = msg.replace(/ai|enter|imes|ober|ufat/g, decryptRules);
 	return decryptedMsg;
 }
 
@@ -84,17 +101,42 @@ function copyToClipboard() {
 	navigator.clipboard.writeText(fraseEncriptada.value);
 }
 
+function doXCrypt(msg) {
+	const constraint = /[^a-zñ\s]/g;
+	if (constraint.test(msg)) {
+		alert('Sólo se permiten letras minúsculas sin acento');
+		return false;
+	}
+	return true;
+}
+
 btnEncriptar.addEventListener('click', (event) => {
 	event.preventDefault();
-	let finalMsg = encrypt();
-	fraseEncriptada.value = finalMsg;
-	frase.value = '';
+	const originalMsg = frase.value;
+	const keepDo = doXCrypt(originalMsg);
+	if (keepDo == true) {
+		const finalMsg = encrypt(originalMsg);
+		fraseEncriptada.style.color = '';
+		fraseEncriptada.value = finalMsg;
+		frase.value = '';
+	} else {
+		fraseEncriptada.style.color = 'red';
+		fraseEncriptada.value = 'Corrige el mensaje de entrada';
+	}
 });
 
 btnDesencriptar.addEventListener('click', (event) => {
 	event.preventDefault();
-	let finalMsg = decrypt();
-	fraseEncriptada.value = finalMsg;
+	const originalMsg = frase.value;
+	const keepDo = doXCrypt(originalMsg);
+	if (keepDo == true) {
+		const finalMsg = decrypt(originalMsg);
+		fraseEncriptada.style.color = '';
+		fraseEncriptada.value = finalMsg;
+	} else {
+		fraseEncriptada.style.color = 'red';
+		fraseEncriptada.value = 'Corrige el mensaje de entrada';
+	}
 });
 
 btnCopiar.addEventListener('click', (event) => {
